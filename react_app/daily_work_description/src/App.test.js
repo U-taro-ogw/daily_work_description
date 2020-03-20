8c948e113dfc427b120f886464034767a9fad80f
U-taro-ogw <51327986+U-taro-ogw@users.noreply.github.com>
2020-03-21 02:53:07 +0900

    create react appでのテスト環境

--- react_app/daily_work_description/src/App.test.js
+++ react_app/daily_work_description/src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+let container = null
+
+beforeEach(() => {
+  container = document.createElement('div')
+})
+
+describe('<App />', () => {
+  it('リンクを出力する', () => {
+    ReactDOM.render(<App />, container)
+    expect(container.innerHTML).toContain('Learn React')
+  })
+})
